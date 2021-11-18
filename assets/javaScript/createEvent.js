28df581e93d8629876239bf894da8cc40c3e22bc
MarcelDurganteDev <info@marceldurgante.dev>
2021-11-18 17:08:38 +0100

    formating

--- assets/javaScript/createEvent.js
+++ assets/javaScript/createEvent.js
@@ -1,130 +1,133 @@
 //TODO create events and save on Local Storage
-
-function eventData(title, startDate, endDate = 0, reminder = 0,  description = '', eventType ) {
+function eventData(
+    title,
+    startDate,
+    endDate = 0,
+    reminder = 0,
+    description = '',
+    eventType
+) {
     return {
         title: title,
-        startDate : startDate,
-        endDate : endDate,
-        reminder : reminder,
+        startDate: startDate,
+        endDate: endDate,
+        reminder: reminder,
         description: description,
-        eventType : eventType
-    }
+        eventType: eventType
+    };
 }
 
-function loadEventBook(){
-    if(localStorage.getItem('eventBook') == null){
+function loadEventBook() {
+    if (localStorage.getItem('eventBook') == null) {
         return [];
     }
-    return JSON.parse(localStorage.getItem('eventBook'))
+    return JSON.parse(localStorage.getItem('eventBook'));
 }
 //TODO convert to object
-function convertToObj(arrayName){
-    let obj
-    if( arrayName.length > 6){
-        obj= eventData(arrayName[0], arrayName[1], arrayName[2], arrayName[3], arrayName[4], arrayName[5])
-    } else{
-        obj= eventData(arrayName[0], arrayName[1], arrayName[1] , arrayName[3], arrayName[4], arrayName[2])
+function convertToObj(arrayName) {
+    let obj;
+    if (arrayName.length > 6) {
+        obj = eventData(
+            arrayName[0],
+            arrayName[1],
+            arrayName[2],
+            arrayName[3],
+            arrayName[4],
+            arrayName[5]
+        );
+    } else {
+        obj = eventData(
+            arrayName[0],
+            arrayName[1],
+            arrayName[1],
+            arrayName[3],
+            arrayName[4],
+            arrayName[2]
+        );
     }
-    return obj
+    return obj;
 }
-
-let eventBook
+let eventBook;
 
 function createEvent() {
-    let arrayData=[]
-    const formClass = document.getElementsByClassName('formInputs')
-    for (num in formClass){
-        arrayData.push(formClass[num].value)
+    let arrayData = [];
+    const formClass = document.getElementsByClassName('formInputs');
+    for (num in formClass) {
+        arrayData.push(formClass[num].value);
         //console.log(formClass[num].value)
     }
-   
     //console.log(eventBook)
-
-    eventBook.push(convertToObj(arrayData))
-
-    localStorage.setItem('eventBook', JSON.stringify(eventBook))
-    closeModal()
+    eventBook.push(convertToObj(arrayData));
+    localStorage.setItem('eventBook', JSON.stringify(eventBook));
+    closeModal();
 }
 
-function deleteEvent(){
-    const formClass = document.getElementsByClassName('formInputs')
-    arrayDel=[]
-    strDel=''
-    for (num in formClass){
-        arrayDel.push(formClass[num].value)
+function deleteEvent() {
+    const formClass = document.getElementsByClassName('formInputs');
+    arrayDel = [];
+    strDel = '';
+    for (num in formClass) {
+        arrayDel.push(formClass[num].value);
     }
-
-    obj = convertToObj(arrayDel)
-    strDel = obj.title + obj.startDate + obj.endDate + obj.reminder + obj.description + obj.eventType;
- 
-    removeEvent(strDel)
-
- }
-
- function removeEvent(eventToRemove){
-    eventBook = eventBook.filter(event => {
-        return !(event.title + event.startDate + event.endDate + event.reminder + event.description + event.eventType == eventToRemove)
-    })
-
-    localStorage.setItem('eventBook', JSON.stringify(eventBook))
-    eventBook =  loadEventBook()
+    obj = convertToObj(arrayDel);
+    strDel =
+        obj.title +
+        obj.startDate +
+        obj.endDate +
+        obj.reminder +
+        obj.description +
+        obj.eventType;
+    removeEvent(strDel);
 }
 
-    // eventBook.push(obj)
-    // arrayData=[]
-    // localStorage.setItem('eventBook', JSON.stringify(eventBook))
-    // closeModal()
-   
-function getDataFromCalendar (num1) {
+function removeEvent(eventToRemove) {
+    eventBook = eventBook.filter(event => {
+        return !(
+            event.title +
+                event.startDate +
+                event.endDate +
+                event.reminder +
+                event.description +
+                event.eventType ==
+            eventToRemove
+        );
+    });
+    localStorage.setItem('eventBook', JSON.stringify(eventBook));
+    eventBook = loadEventBook();
+}
+
+function getDataFromCalendar(num1) {
     //COGER LOS EVENTOS DEL DIA
-    
-    filter= eventBook.filter(element=>{
+    filter = eventBook.filter(element => {
         // console.log(new Date(fecha).getFullYear(), new Date(element.startDate).getFullYear())
-        if(new Date(fecha).getFullYear() == new Date(element.startDate).getFullYear()){
-            if(new Date(fecha).getMonth() == new Date(element.startDate).getMonth()){
-            // console.log(new Date(fecha).getDate(), new Date(element.startDate).getDate())
-                if(new Date(fecha).getDate() == new Date(element.startDate).getDate()){
-                    return true
+        if (
+            new Date(fecha).getFullYear() ==
+            new Date(element.startDate).getFullYear()
+        ) {
+            if (
+                new Date(fecha).getMonth() ==
+                new Date(element.startDate).getMonth()
+            ) {
+                // console.log(new Date(fecha).getDate(), new Date(element.startDate).getDate())
+                if (
+                    new Date(fecha).getDate() ==
+                    new Date(element.startDate).getDate()
+                ) {
+                    return true;
                 }
             }
-        } 
-        return false
-    })
+        }
+        return false;
+    });
 
     filter.forEach(event => {
-        eventDay = new Date(event.startDate).getDate()
-        eventMonth = new Date(event.startDate).getMonth()
-        eventYear =  new Date(event.startDate).getFullYear()
-       
-        console.log(eventDay)
-        console.log(eventMonth +1)
-        console.log(eventYear)
-        console.log(event.title)
-console.log(filter)  
-        let eventOnCalendar = document.createElement('div')
-        let eventText = document.createElement('h3') 
-        eventText.innerHTML = event.title
-        num1.appendChild(eventText)
-
-    /*     if (element.startDate == fecha){
-            
-        } */
+        eventDay = new Date(event.startDate).getDate();
+        eventMonth = new Date(event.startDate).getMonth();
+        eventYear = new Date(event.startDate).getFullYear();
+        let eventText = document.createElement('h3');
+        eventText.innerHTML = event.title;
+        num1.appendChild(eventText);
+        
     });
-    // console.log(filter)
-    //CREAR UN IF SI LA LONGITUD ES 0 Y NO LEAS EL RESTO DEL CODIGO
-    //COGER LA VARIABLE CREADA Y IMPRIMIRLA
-//     eventBook.forEach(element => {
-//     title=element.title
-//     date=new Date(element.startDate)
-//     finalDate=new Date(element.endDate)
-//     console.log(finalDate)
-//    })
 }
-// eventBook.forEach(element => {
-//     prueba=element.title
-//     console.log(prueba)
-// });
-   // crear otra funcion donde dev criar um div e ponder este div con este titotlo y esta data. e ponder este dive dentro del dia del calendario 
-
- 
 
--- assets/javaScript/modalContent.js
+++ assets/javaScript/modalContent.js
@@ -1,22 +1,22 @@
-const modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
 
-const modalContentor = document.getElementsByClassName('modal-content')
-var modalContent = modalContentor[0]
+const modalContentor = document.getElementsByClassName('modal-content');
+var modalContent = modalContentor[0];
 //TODO Display modal Start
 function modalStart() {
-    beginModal()
+    beginModal();
     fecha = undefined;
-    openModal()
+    openModal();
 }
 
 //TODO modal open
 function openModal() {
-    modal.style.display = "block";
+    modal.style.display = 'block';
 }
 
 //TODO modal close
 function closeModal() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     // cleanModal()
 }
 
@@ -30,66 +30,68 @@ function cleanModal() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
-        modal.style.display = "none";
-        cleanModal()
+        modal.style.display = 'none';
+        cleanModal();
     }
-}
+};
 
 function displayForm() {
-    (endDateCheck.checked == true) ? createHideElements() : cleanHideElements();
+    endDateCheck.checked == true ? createHideElements() : cleanHideElements();
 }
 
-function displayReminder(){
-    (reminder.checked == true) ? createReminder() : cleanReminder();
+function displayReminder() {
+    reminder.checked == true ? createReminder() : cleanReminder();
 }
 
-function createHideElements(){
-    content="<div><label for='endDate'>End Date</label><input type='datetime-local' name='End Date'  class='formInputs' id='endDate'> </div>"
+function createHideElements() {
+    content =
+        "<div><label for='endDate'>End Date</label><input type='datetime-local' name='End Date'  class='formInputs' id='endDate'> </div>";
 
-    content+="<div id='checkboxRemind'><label for='reminder'> Remind me when event Start</label><input type='checkbox' name='reminder' id='reminder'> </div>"
+    content +=
+        "<div id='checkboxRemind'><label for='reminder'> Remind me when event Start</label><input type='checkbox' name='reminder' id='reminder'> </div>";
 
-    content+="<div id='divReminder'></div>"
+    content += "<div id='divReminder'></div>";
 
-    content+="<div><label for='description'>Description</label> <textarea name='description' class='formInputs' id='description' cols='20' rows='5'></textarea></div>"
+    content +=
+        "<div><label for='description'>Description</label> <textarea name='description' class='formInputs' id='description' cols='20' rows='5'></textarea></div>";
 
-    
+    hideForm[0].innerHTML = content;
 
-    hideForm[0].innerHTML= content
-
-    const reminder= document.getElementById('reminder')
-    reminder.addEventListener('click', displayReminder)
+    const reminder = document.getElementById('reminder');
+    reminder.addEventListener('click', displayReminder);
 }
 
-function createReminder(){
-    const divReminder= document.getElementById('divReminder')
+function createReminder() {
+    const divReminder = document.getElementById('divReminder');
 
-    content= "<label for='remindTimer'> Time reminder </label> <select name='remindTimer' class='formInputs' id='remindTimer'> <option value='5'> 5 Minutes</option> <option value='10'> 10 Minutes</option> <option value='15'> 15 Minutes</option> <option value='30'> 30 Minutes</option> <option value='60'> 1 hour</option> </select> "
+    content =
+        "<label for='remindTimer'> Time reminder </label> <select name='remindTimer' class='formInputs' id='remindTimer'> <option value='5'> 5 Minutes</option> <option value='10'> 10 Minutes</option> <option value='15'> 15 Minutes</option> <option value='30'> 30 Minutes</option> <option value='60'> 1 hour</option> </select> ";
 
-    divReminder.innerHTML= content
+    divReminder.innerHTML = content;
 }
 
-function cleanHideElements(){
+function cleanHideElements() {
     while (hideForm[0].firstChild) {
         hideForm[0].removeChild(hideForm[0].lastChild);
     }
 }
 
-function cleanReminder(){
-    cont= 0
-    divElements= divReminder.childElementCount + 1;
-    while(divElements > cont ){
-        console.log(divElements, cont)
-        divReminder.removeChild(divReminder.firstChild)
-        cont++
+function cleanReminder() {
+    cont = 0;
+    divElements = divReminder.childElementCount + 1;
+    while (divElements > cont) {
+        console.log(divElements, cont);
+        divReminder.removeChild(divReminder.firstChild);
+        cont++;
     }
 }
 
-window.onkeydown = (key)=>{
-    escape(key)
-}
+window.onkeydown = key => {
+    escape(key);
+};
 
-function escape (key){
-    if (key.code == 'Escape'){
-        closeModal()
+function escape(key) {
+    if (key.code == 'Escape') {
+        closeModal();
     }
 }
